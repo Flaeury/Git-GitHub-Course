2c76aa9f716eae21e703b78eafdb4c21b1b8ea95
saymon <danielsaymon31@gmai.com>
2024-02-27 16:42:03 -0300

    adicionado numeros as senhas

--- password.py
+++ password.py
@@ -1,26 +1,22 @@
 import random
 
-# Define os caracteres para a nova senha
+
 minusculo = "abcdefghijklmnopqrstuvxyz"
 maiusculo = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
 especiais = "!_-.;:[]()"
+numeros = "0123456789"
 
-# -------------------------------------------------------------
-# AGORA, VOCÊ DEVE ADICIONAR CARACTERES NUMÉRIOS PARA A SENHA.
-# -------------------------------------------------------------
 
-# Junta todos os caracteres em uma única string
-tudo = minusculo + maiusculo + especiais
-tamanho = 12  # Tamanho da senha
+tudo = minusculo + maiusculo + especiais + numeros
+tamanho = 12  
 
-# Inicializa a senha como uma string vazia
+
 senha = ""
 
-# Gera a senha selecionando aleatoriamente caracteres da string "tudo"
 for _ in range(tamanho):
     senha += random.choice(tudo)
 
-# OUTPUT
+
 print("=========================================")
 print("\nTa aqui a senha seu preguiçoso:", senha)
 print("=========================================")
