0a68a3ec0a4ab60fcad0ec94a1aef4eed8b74c19
vanessa <vanessapedrosa1213@gmail.com>
2024-02-27 16:41:13 -0300

    numeros adicionados

--- password.py
+++ password.py
@@ -4,13 +4,13 @@ import random
 minusculo = "abcdefghijklmnopqrstuvxyz"
 maiusculo = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
 especiais = "!_-.;:[]()"
-
+numeros = "1234567890"
 # -------------------------------------------------------------
 # AGORA, VOCÊ DEVE ADICIONAR CARACTERES NUMÉRIOS PARA A SENHA.
 # -------------------------------------------------------------
 
 # Junta todos os caracteres em uma única string
-tudo = minusculo + maiusculo + especiais
+tudo = minusculo + maiusculo + especiais + numeros
 tamanho = 12  # Tamanho da senha
 
 # Inicializa a senha como uma string vazia
@@ -22,5 +22,5 @@ for _ in range(tamanho):
 
 # OUTPUT
 print("=========================================")
-print("\nTa aqui a senha seu preguiçoso:", senha)
+print("\nTa aqui a senha, seu preguiçoso:", senha)
 print("=========================================")
